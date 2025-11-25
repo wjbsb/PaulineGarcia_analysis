dd4e78df994519236d3ad65220e44bec3e60829a
wjbsb <39159239+wjbsb@users.noreply.github.com>
2025-11-25 15:06:05 +0100

    Add files via upload

---
+++ ChIPseq_1/launch_chipseq.sh
@@ -0,0 +1,8 @@
+
+
+
+ 
+nextflow run nf-core/chipseq -r 2.0.0 -resume pauline_garcia_06122023_bwa -profile singularity -work-dir /home/mna_bioinfo/Bureau/work/MAIN/chipseq -params-file nf-params.json
+
+
+
