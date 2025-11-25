1d827ac5d7c7f3021bad12ca4252ab727fbde371
wjbsb <39159239+wjbsb@users.noreply.github.com>
2025-11-25 15:05:33 +0100

    Add files via upload

---
+++ ChIPseq_2/launch_chipseqREP1.sh
@@ -0,0 +1,8 @@
+
+
+
+ 
+nextflow run nf-core/chipseq -r 2.0.0 -name pauline_garcia_06122023_bwa_REP1 -profile singularity -work-dir /home/mna_bioinfo/Bureau/work/MAIN/chipseqREP1 -params-file nf-paramsREP1.json
+
+
+
